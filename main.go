c37f0adaf797b4de1ae5a0cab8df547c78758e15
Norbert Fuhs <norbert.fuhs.xyz@gmail.com>
2020-02-08 20:57:12 +0100

    added player.go, res/sprites/spaceship-small.bmp

--- main.go
+++ main.go
@@ -1,7 +1,59 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/veandco/go-sdl2/sdl"
+)
+
+const (
+	screenWidth  = 600
+	screenHeight = 800
+)
 
 func main() {
-	fmt.Println("Space Gopher")
+	if err := sdl.Init(sdl.INIT_EVERYTHING); err != nil {
+		fmt.Println("initializing SDL:", err)
+		return
+	}
+
+	window, err := sdl.CreateWindow(
+		"Space Gopher",
+		sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
+		screenWidth, screenHeight,
+		sdl.WINDOW_OPENGL)
+	if err != nil {
+		fmt.Println("initializing window:", err)
+		return
+	}
+	defer window.Destroy()
+
+	renderer, err := sdl.CreateRenderer(window, -1, sdl.RENDERER_ACCELERATED)
+	if err != nil {
+		fmt.Println("initializing renderer:", err)
+		return
+	}
+	defer renderer.Destroy()
+
+	plr, err := newPlayer(renderer)
+	if err != nil {
+		fmt.Println("creating player:", err)
+		return
+	}
+
+	for {
+		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
+			switch event.(type) {
+			case *sdl.QuitEvent:
+				return
+			}
+		}
+
+		renderer.SetDrawColor(255, 255, 255, 255)
+		renderer.Clear()
+
+		plr.draw(renderer)
+
+		renderer.Present()
+	}
 }
